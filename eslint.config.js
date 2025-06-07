25be02c6d33d64f3c855535d79315e53e09027cd
沈可 <k-chin@advanced-media.co.jp>
2025-06-07 17:20:01 +0900

    [pnpm] add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: globals.browser } },
-  pluginVue.configs["flat/essential"],
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginVue.configs['flat/essential'],
+  eslintConfigPrettier,
+  globalIgnores(['dist/']),
 ]);
